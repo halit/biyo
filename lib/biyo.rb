a25a54523f10d7668311ca75bc07f9c938f5c3c9
Halit Alptekin <info@halitalptekin.com>
2013-08-26 01:20:38 +0300

    #3 create project class and pass first test

--- lib/biyo.rb
+++ lib/biyo.rb
@@ -1,5 +1,22 @@
 require 'biyo/version'
+require 'json'
 
 module Biyo
-  # Your code goes here...
+  class Bio
+
+    def initialize
+      @collector = Hash.new
+    end
+
+    def new_field field_parameter
+      field_parameter.each do |key, value|
+        @collector[key] = value
+      end
+
+    end
+
+    def output
+      @collector.to_json
+    end
+  end
 end
